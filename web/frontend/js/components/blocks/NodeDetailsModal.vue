280d3c940e4259750045bf2b9526ceb4a801e236
et-nik <nikita.hldm@gmail.com>
2026-05-13 01:22:30 +0200

    ui: update node details modal

--- web/frontend/js/components/blocks/NodeDetailsModal.vue
+++ web/frontend/js/components/blocks/NodeDetailsModal.vue
@@ -36,7 +36,7 @@
     </template>
 
     <template #header-extra>
-      <div class="flex flex-wrap gap-1.5 mr-3">
+      <div class="flex flex-wrap gap-1.5 mr-10">
         <GButton
             color="blue"
             size="small"
